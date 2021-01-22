33a575463f13d5d3912ac64db33e49d6f7580972
Mirco De Marchi <mirco.demarchi@studenti.univr.it>
2021-01-22 22:20:33 +0100

    Update image generation

--- src/main.py
+++ src/main.py
@@ -79,6 +79,10 @@ def scooter_trajectories(config: configparser.SectionProxy, log_lvl):
     if config.getboolean("load-generated-data"):
         st.load_generated()
 
+    if st.dataset.empty:
+        return
+
+    # ML
     if config.getboolean("perform-timedelta-heuristic") or config.getboolean("perform-spreaddelta-heuristic")\
             or config.getboolean("perform-edgedelta-heuristic") or config.getboolean("perform-coorddelta-heuristic"):
         groupby = STC.POS_GEN_RENTAL_ID_CN
@@ -97,98 +101,107 @@ def scooter_trajectories(config: configparser.SectionProxy, log_lvl):
                                     groupby=groupby)
         st.to_csv()
 
-    if not st.dataset.empty:
-        st.print_stats()
+    if config.getboolean("perform-kmeans"):
+        dataset_for_clustering, cols = st.prepare_for_clustering()
+        #dataset_for_clustering = dataset_for_clustering.loc[dataset_for_clustering[STC.MERGE_POS_LATITUDE_CN] >= 44.0]
+        kmeans = Clustering(dataset_for_clustering, STC.CLUSTERING_COLS, dataset_name="ScooterTrajectories")
 
-        if config.getboolean("perform-analysis"):
-            # Rental analysis
-            da_rental = DataAnalysis(st.rental, STC.RENTAL_ANALYSIS_COLS,
-                                     dataset_name="ScooterTrajectories",
-                                     save_file=True, prefix="rental")
-            da_rental.show_distributions().show_2d_distributions(STC.RENTAL_2D_ANALYSIS_COUPLES)
-            for c in STC.RENTAL_2D_ANALYSIS_COUPLES:
-                da_rental.show_joint(on=c)
+        if config["n-clusters"] is None:
+            kmeans.test(standardize=config.getboolean("with-standardization"), pca=config.getboolean("with-pca"))
+            kmeans.show_wcss()
+        else:
+            kmeans.exec(config.getint("n-clusters"), standardize=config.getboolean("with-standardization"),
+                        pca=config.getboolean("with-pca"))
+            dataset_for_clustering["labels"] = kmeans.labels
+            # All dataset k-means analysis
+            da_dataset = DataAnalysis(dataset_for_clustering, cols, dataset_name="ScooterTrajectories",
+                                      save_file=True, prefix="kmeans_all")
+            da_dataset.show_joint(on=[STC.MERGE_POS_LONGITUDE_CN, STC.MERGE_POS_LATITUDE_CN, "labels"])
 
-            # Pos analysis
-            if config["num-of-pos-to-analyze"] is None:
-                pos_to_analyze = st.pos
-            else:
-                pos_to_analyze = st.pos.iloc[:config.getint("num-of-pos-to-analyze")]
-
-            da_pos = DataAnalysis(pos_to_analyze, STC.POS_GEN_ANALYSIS_COLS,
-                                  dataset_name="ScooterTrajectories", save_file=True, prefix="pos")
-            da_pos.show_distributions().show_2d_distributions(STC.POS_GEN_2D_ANALYSIS_COUPLES)
-            for c in STC.POS_GEN_2D_ANALYSIS_COUPLES:
-                da_pos.show_joint(on=c)
-
-            # Result analysis
-            rental_to_analyze = st.pos[STC.POS_GEN_RENTAL_ID_CN].unique()[
+            rental_to_analyze = dataset_for_clustering[STC.MERGE_RENTAL_ID_CN].unique()[
                                 :config.getint("num-of-rental-in-dataset-to-analyze")]
-            dataset_to_analyze = st.pos.loc[st.pos[STC.POS_GEN_RENTAL_ID_CN].isin(rental_to_analyze)]
-            da_dataset = DataAnalysis(dataset_to_analyze, st.pos.columns, dataset_name="ScooterTrajectories",
-                                      save_file=True, prefix="dataset_all")
-            da_dataset.show_line(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
+            dataset_to_analyze = dataset_for_clustering.loc[
+                dataset_for_clustering[STC.MERGE_RENTAL_ID_CN].isin(rental_to_analyze)]
+            # Bottom left dataset k-means analysis
+            dataset_bl = dataset_to_analyze.loc[dataset_to_analyze[STC.MERGE_POS_LATITUDE_CN] < 44.0]
+            da_dataset_bl = DataAnalysis(dataset_bl, dataset_bl.columns, dataset_name="ScooterTrajectories",
+                                         save_file=True, prefix="kmeans_bottom_left")
+            da_dataset_bl.show_joint(on=[STC.MERGE_POS_LONGITUDE_CN, STC.MERGE_POS_LATITUDE_CN, "labels"])
+            # Top right dataset k-means analysis
+            dataset_tr = dataset_to_analyze.loc[dataset_to_analyze[STC.MERGE_POS_LATITUDE_CN] >= 44.0]
+            da_dataset_tr = DataAnalysis(dataset_tr, dataset_tr.columns, dataset_name="ScooterTrajectories",
+                                         save_file=True, prefix="kmeans_top_right")
+            da_dataset_tr.show_joint(on=[STC.MERGE_POS_LONGITUDE_CN, STC.MERGE_POS_LATITUDE_CN, "labels"])
 
-            dataset_bl = dataset_to_analyze.loc[dataset_to_analyze[STC.POS_GEN_LATITUDE_CN] < 44.0]
-            da_dataset_bl = DataAnalysis(dataset_bl, st.pos.columns, dataset_name="ScooterTrajectories",
-                                         save_file=True, prefix="dataset_bottom_left")
-            da_dataset_bl.show_line(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
-            da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
-            da_dataset_bl.show_line(on=STC.POS_GEN_OVER_TIMEDELTA_ANALYSIS_TUPLE)
-            da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE)
-            da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE)
-            da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE)
+    # Analysis
+    st.print_stats()
 
-            dataset_tr = dataset_to_analyze.loc[dataset_to_analyze[STC.POS_GEN_LATITUDE_CN] >= 44.0]
-            da_dataset_tr = DataAnalysis(dataset_tr, st.pos.columns, dataset_name="ScooterTrajectories",
-                                         save_file=True, prefix="dataset_top_right")
-            da_dataset_tr.show_line(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
-            da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
-            da_dataset_tr.show_line(on=STC.POS_GEN_OVER_TIMEDELTA_ANALYSIS_TUPLE)
-            da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE)
-            da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE)
-            da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE)
+    pos_to_analyze = st.pos if config["num-of-pos-to-analyze"] is None else st.pos.iloc[
+                                                                            :config.getint("num-of-pos-to-analyze")]
+    rental_to_analyze = st.pos[STC.POS_GEN_RENTAL_ID_CN].unique()[
+                        :config.getint("num-of-rental-in-dataset-to-analyze")]
+    dataset_to_analyze = st.pos.loc[st.pos[STC.POS_GEN_RENTAL_ID_CN].isin(rental_to_analyze)]
+    dataset_bl = dataset_to_analyze.loc[dataset_to_analyze[STC.POS_GEN_LATITUDE_CN] < 44.0]
+    dataset_tr = dataset_to_analyze.loc[dataset_to_analyze[STC.POS_GEN_LATITUDE_CN] >= 44.0]
+    if config.getboolean("perform-data-analysis"):
+        # Rental analysis
+        da_rental = DataAnalysis(st.rental, STC.RENTAL_ANALYSIS_COLS,
+                                 dataset_name="ScooterTrajectories",
+                                 save_file=True, prefix="rental")
+        da_rental.show_distributions().show_2d_distributions(STC.RENTAL_2D_ANALYSIS_COUPLES)
+        for c in STC.RENTAL_2D_ANALYSIS_COUPLES:
+            da_rental.show_joint(on=c)
 
-        if config.getboolean("perform-map"):
-            rental_to_analyze = st.pos[STC.POS_GEN_RENTAL_ID_CN].unique()[
-                                :config.getint("num-of-rental-in-dataset-to-analyze")]
-            dataset_to_analyze = st.pos.loc[st.pos[STC.POS_GEN_RENTAL_ID_CN].isin(rental_to_analyze)]
-            da_dataset = DataAnalysis(dataset_to_analyze, st.pos.columns, dataset_name="ScooterTrajectories",
-                                      save_file=True)
-            da_dataset.show_line_map(on=STC.POS_GEN_OVER_RENTAL_MAP_TUPLE,
-                                     hover_data=STC.POS_GEN_OVER_RENTAL_MAP_HOVER_DATA)
-            da_dataset.show_scatter_map(on=STC.POS_GEN_OVER_CLUSTER_MAP_TUPLE,
-                                        hover_data=STC.POS_GEN_OVER_CLUSTER_MAP_HOVER_DATA)
+        # Pos analysis
+        da_pos = DataAnalysis(pos_to_analyze, STC.POS_GEN_ANALYSIS_COLS,
+                              dataset_name="ScooterTrajectories", save_file=True, prefix="pos")
+        da_pos.show_distributions().show_2d_distributions(STC.POS_GEN_2D_ANALYSIS_COUPLES)
+        for c in STC.POS_GEN_2D_ANALYSIS_COUPLES:
+            da_pos.show_joint(on=c)
 
-        if config.getboolean("perform-kmeans"):
-            st.time_to_float()
-            kmeans = Clustering(st.merge, STC.MERGE_CLUSTERING_COLS, dataset_name="ScooterTrajectories")
+        # Result analysis
+        da_dataset = DataAnalysis(dataset_to_analyze, st.pos.columns, dataset_name="ScooterTrajectories",
+                                  save_file=True, prefix="data_all")
+        da_dataset.show_line(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
 
-            if config["n-clusters"] is None:
-                kmeans.test(standardize=config.getboolean("with-standardization"), pca=config.getboolean("with-pca"))
-                kmeans.show_wcss()
-            else:
-                kmeans.exec(config.getint("n-clusters"), standardize=config.getboolean("with-standardization"),
-                            pca=config.getboolean("with-pca"))
-                st.merge["labels"] = kmeans.labels
-                # All dataset k-means analysis
-                da_dataset = DataAnalysis(st.merge, st.merge.columns, dataset_name="ScooterTrajectories",
-                                          save_file=True, prefix="kmeans_all")
-                da_dataset.show_joint(on=[STC.MERGE_POS_LONGITUDE_CN, STC.MERGE_POS_LATITUDE_CN, "labels"])
+        da_dataset_bl = DataAnalysis(dataset_bl, st.pos.columns, dataset_name="ScooterTrajectories",
+                                     save_file=True, prefix="data_bottom_left")
+        da_dataset_bl.show_line(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
+        da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
 
-                rental_to_analyze = st.merge[STC.MERGE_RENTAL_ID_CN].unique()[
-                                    :config.getint("num-of-rental-in-dataset-to-analyze")]
-                dataset_to_analyze = st.merge.loc[st.merge[STC.MERGE_RENTAL_ID_CN].isin(rental_to_analyze)]
-                # Bottom left dataset k-means analysis
-                dataset_bl = dataset_to_analyze.loc[dataset_to_analyze[STC.POS_GEN_LATITUDE_CN] < 44.0]
-                da_dataset_bl = DataAnalysis(dataset_bl, dataset_bl.columns, dataset_name="ScooterTrajectories",
-                                             save_file=True, prefix="kmeans_bottom_left")
-                da_dataset_bl.show_joint(on=[STC.MERGE_POS_LONGITUDE_CN, STC.MERGE_POS_LATITUDE_CN, "labels"])
-                # Top right dataset k-means analysis
-                dataset_tr = dataset_to_analyze.loc[dataset_to_analyze[STC.MERGE_POS_LATITUDE_CN] >= 44.0]
-                da_dataset_tr = DataAnalysis(dataset_tr, dataset_tr.columns, dataset_name="ScooterTrajectories",
-                                             save_file=True, prefix="kmeans_top_right")
-                da_dataset_tr.show_joint(on=[STC.MERGE_POS_LONGITUDE_CN, STC.MERGE_POS_LATITUDE_CN, "labels"])
+        da_dataset_tr = DataAnalysis(dataset_tr, st.pos.columns, dataset_name="ScooterTrajectories",
+                                     save_file=True, prefix="data_top_right")
+        da_dataset_tr.show_line(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
+        da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE)
+
+    if config.getboolean("perform-heuristic-analysis"):
+        da_dataset_bl = DataAnalysis(dataset_bl, st.pos.columns, dataset_name="ScooterTrajectories",
+                                     save_file=True, prefix="heuristic_bottom_left")
+        da_dataset_bl.show_line(on=STC.POS_GEN_OVER_TIMEDELTA_ANALYSIS_TUPLE)
+        da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE)
+        da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE)
+        da_dataset_bl.show_joint(on=STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE)
+
+        da_dataset_tr = DataAnalysis(dataset_tr, st.pos.columns, dataset_name="ScooterTrajectories",
+                                     save_file=True, prefix="heuristic_top_right")
+        da_dataset_tr.show_line(on=STC.POS_GEN_OVER_TIMEDELTA_ANALYSIS_TUPLE)
+        da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE)
+        da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE)
+        da_dataset_tr.show_joint(on=STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE)
+
+    if config.getboolean("perform-clustering-analysis"):
+        pass
+
+    if config.getboolean("perform-map"):
+        rental_to_analyze = st.pos[STC.POS_GEN_RENTAL_ID_CN].unique()[
+                            :config.getint("num-of-rental-in-dataset-to-analyze")]
+        dataset_to_analyze = st.pos.loc[st.pos[STC.POS_GEN_RENTAL_ID_CN].isin(rental_to_analyze)]
+        da_dataset = DataAnalysis(dataset_to_analyze, st.pos.columns, dataset_name="ScooterTrajectories",
+                                  save_file=True)
+        da_dataset.show_line_map(on=STC.POS_GEN_OVER_RENTAL_MAP_TUPLE,
+                                 hover_data=STC.POS_GEN_OVER_RENTAL_MAP_HOVER_DATA)
+        da_dataset.show_scatter_map(on=STC.POS_GEN_OVER_CLUSTER_MAP_TUPLE,
+                                    hover_data=STC.POS_GEN_OVER_CLUSTER_MAP_HOVER_DATA)
 
 
 def main():
