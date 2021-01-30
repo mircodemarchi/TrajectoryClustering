b61fa4e2a8e7f7f5e1f27af6c15135cde48ec33f
Mirco De Marchi <mirco.demarchi@studenti.univr.it>
2021-01-30 22:25:29 +0100

    Divide cluster analysis from clustering execution

--- src/test/scooter_trajectories_test.py
+++ src/test/scooter_trajectories_test.py
@@ -17,14 +17,19 @@ SAVE_FILE = False
 
 RENTAL_IMG_FN_PREFIX = "rental"
 POS_IMG_FN_PREFIX = "pos"
+POS_OVER_RENTAL_IMG_FN_PREFIX = "data_all"
 CLUSTER_IMG_FN_PREFIX = "kmeans"
+CLUSTER_TEST_IMG_FN_PREFIX = "kmeans_test"
 HEURISTIC_IMG_FN_PREFIX = "heuristic"
+
+FORCE_RENTAL_NUM_TO_ANALYZE = 100
 
 
 class ScooterTrajectoriesTest:
     def __init__(self, log_lvl=None, chunk_size=None, max_chunk_num=None, rental_num_to_analyze=None,
                  timedelta=None, spreaddelta=None, edgedelta=None, group_on_timedelta=True,
-                 n_clusters=None, with_pca=True, with_standardization=True, only_north=False):
+                 n_clusters=None, with_pca=False, with_standardization=False, with_normalization=False,
+                 only_north=False):
         self.st = ScooterTrajectoriesDS(log_lvl=log_lvl)
         # Generation settings
         self.chunk_size = chunk_size
@@ -39,11 +44,13 @@ class ScooterTrajectoriesTest:
         self.n_clusters = n_clusters
         self.with_pca = with_pca
         self.with_standardization = with_standardization
+        self.with_normalization = with_normalization
         self.only_north = only_north
 
         # Others
-        self.load_done = False
-        self.heuristic_done = False
+        self.clustering_done = False
+        self.data_clusterized = None
+        self.partitions_clusterized = None
         self.groupby = [STC.POS_GEN_RENTAL_ID_CN,
                         STC.POS_GEN_TIMEDELTA_ID_CN] if group_on_timedelta else STC.POS_GEN_RENTAL_ID_CN
 
@@ -158,16 +165,19 @@ class ScooterTrajectoriesTest:
         return self
 
     def is_data_processed(self):
-        return self.load_done or not self.st.empty()
+        return not self.st.empty()
 
     def is_heuristic_processed(self):
-        return self.heuristic_done or not self.st.heuristic_empty()
+        return not self.st.heuristic_empty()
+
+    def is_clustering_processed(self):
+        return self.clustering_done and self.data_clusterized is not None and self.partitions_clusterized is not None
 
     def heuristic(self):
         log.d("Test {} start heuristic process".format(DATASET_NAME))
         if self.is_heuristic_processed():
-            log.d("Test {} heuristic: columns already created".format(DATASET_NAME))
-            return self
+            log.w("Test {} heuristic: columns already exist, overwriting them".format(DATASET_NAME))
+
         if not self.is_data_processed():
             log.e("Test {} heuristic: you have to process data earlier".format(DATASET_NAME))
             return self
@@ -175,31 +185,31 @@ class ScooterTrajectoriesTest:
         self.st.spreaddelta_heuristic(spreaddelta=self.spreaddelta, groupby=self.groupby)
         self.st.edgedelta_heuristic(edgedelta=self.edgedelta, groupby=self.groupby)
         self.st.coorddelta_heuristic(spreaddelta=self.spreaddelta, edgedelta=self.edgedelta, groupby=self.groupby)
-        self.heuristic_done = True
         return self
 
-    def test_clustering(self, prefix=None):
+    def test_clustering(self):
+        prefix = CLUSTER_TEST_IMG_FN_PREFIX
         log.d("Test {} start clustering wcss test".format(DATASET_NAME))
         if not self.is_heuristic_processed():
             log.e("Test {} test_clustering: you have to process heuristic earlier".format(DATASET_NAME))
             return self
-        prefix = "" if prefix is None else "{}_".format(prefix)
         dataset_for_clustering = self.__prepare()
         partitions = self.__partition(dataset_for_clustering, only_north=self.only_north)
 
         # Perform clustering tests
         kmeans = Clustering(dataset_for_clustering, STC.CLUSTERING_COLS, dataset_name="ScooterTrajectories")
-        kmeans.test(range_clusters=range(1, 20), standardize=self.with_standardization, pca=self.with_pca)
+        kmeans.test(range_clusters=range(1, 20), standardize=self.with_standardization,
+                    normalize=self.with_normalization,  pca=self.with_pca, components=STC.CLUSTERING_COMPONENTS)
         kmeans.show_wcss(save_file=SAVE_FILE, prefix=prefix + "all")
         for key in partitions:
             kmeans = Clustering(partitions[key], STC.CLUSTERING_COLS, dataset_name="ScooterTrajectories")
-            kmeans.test(range_clusters=range(1, 30), standardize=self.with_standardization, pca=self.with_pca)
+            kmeans.test(range_clusters=range(1, 30), standardize=self.with_standardization,
+                        normalize=self.with_normalization, pca=self.with_pca, components=STC.CLUSTERING_COMPONENTS)
             kmeans.show_wcss(save_file=SAVE_FILE, prefix=prefix + key)
 
     def clustering(self):
-        prefix = CLUSTER_IMG_FN_PREFIX
         if self.n_clusters is None:
-            self.test_clustering(prefix)
+            self.test_clustering()
             return self
 
         log.d("Test {} start clustering k-means analysis".format(DATASET_NAME))
@@ -207,66 +217,121 @@ class ScooterTrajectoriesTest:
             log.e("Test {} clustering: you have to process heuristic earlier".format(DATASET_NAME))
             return self
 
-        prefix = "" if prefix is None else "{}_".format(prefix)
+        components = STC.CLUSTERING_COMPONENTS  # STC.CLUSTERING_COMPONENTS or None or a number
         dataset_for_clustering = self.__prepare()
-        partitions = self.__partition(dataset_for_clustering, only_north=self.only_north)
 
         # Perform clustering in relation to the entire data
         kmeans = Clustering(dataset_for_clustering, STC.CLUSTERING_COLS, dataset_name="ScooterTrajectories")
-        kmeans.exec(self.n_clusters, standardize=self.with_standardization, pca=self.with_pca)
+        kmeans.exec(self.n_clusters, standardize=self.with_standardization, normalize=self.with_normalization,
+                    pca=self.with_pca, components=components)
         dataset_for_clustering[STC.CLUSTER_ID_CN] = kmeans.labels
-        self.__line_joint_analysis(self.__filter(dataset_for_clustering), joint_list=[STC.CLUSTER_ANALYSIS_TUPLE],
-                                   prefix=prefix + "all")
-        self.__cardinal_analysis(dataset_for_clustering, joint_list=[STC.CLUSTER_ANALYSIS_TUPLE],
-                                 prefix=prefix + "all", skip_filter=False)
 
         # Perform clustering in relation to each partition
+        partitions = self.__partition(dataset_for_clustering, only_north=self.only_north)
         for key in partitions:
             kmeans = Clustering(partitions[key], STC.CLUSTERING_COLS, dataset_name="ScooterTrajectories")
-            kmeans.exec(self.n_clusters, standardize=self.with_standardization, pca=self.with_pca)
+            kmeans.exec(self.n_clusters, standardize=self.with_standardization, normalize=self.with_normalization,
+                        pca=self.with_pca, components=components)
             partitions[key][STC.CLUSTER_ID_CN] = kmeans.labels
-            self.__line_joint_analysis(self.__filter(partitions[key]), joint_list=[STC.CLUSTER_ANALYSIS_TUPLE],
-                                       prefix=prefix + key)
+
+        self.data_clusterized = dataset_for_clustering
+        self.partitions_clusterized = partitions
+        self.clustering_done = True
 
     def generated_data_analysis(self):
         log.d("Test {} generated data analysis".format(DATASET_NAME))
+        if not self.is_data_processed():
+            log.e("Test {} generated data analysis: you have to process data earlier".format(DATASET_NAME))
+            return self
+
         # Rental analysis
-        # rental_to_analyze = self.st.rental
-        # if self.rental_num_to_analyze is not None:
-        #     rental_to_analyze = rental_to_analyze.iloc[:self.rental_num_to_analyze]
-        # self.__overview_analysis(rental_to_analyze, STC.RENTAL_ANALYSIS_COLS, STC.RENTAL_2D_ANALYSIS_COUPLES,
-        #                          RENTAL_IMG_FN_PREFIX)
-        # # Pos analysis
-        pos_to_analyze = self.__filter(self.st.pos)
-        # self.__overview_analysis(pos_to_analyze, STC.POS_GEN_ANALYSIS_COLS, STC.POS_GEN_2D_ANALYSIS_COUPLES,
-        #                          POS_IMG_FN_PREFIX)
+        rental_to_analyze = self.__rental_filter(self.st.rental)
+        if self.rental_num_to_analyze is not None:
+            rental_to_analyze = rental_to_analyze.iloc[:self.rental_num_to_analyze]
+        self.__overview_analysis(rental_to_analyze, STC.RENTAL_ANALYSIS_COLS, STC.RENTAL_2D_ANALYSIS_COUPLES,
+                                 RENTAL_IMG_FN_PREFIX)
+        # Pos analysis
+        pos_to_analyze = self.__filter(self.__pos_filter(self.st.pos))
+        self.__overview_analysis(pos_to_analyze, STC.POS_GEN_ANALYSIS_COLS, STC.POS_GEN_2D_ANALYSIS_COUPLES,
+                                 POS_IMG_FN_PREFIX)
+        self.__cardinal_analysis(self.st.pos, prefix=POS_IMG_FN_PREFIX,
+                                 line_list=[STC.POS_GEN_2D_ANALYSIS_COUPLES[0]],
+                                 joint_list=[STC.POS_GEN_2D_ANALYSIS_COUPLES[0]])
         # Pos over Rental analysis
-        self.__line_joint_analysis(pos_to_analyze, line_list=[STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE],
-                                   prefix="data_all")
+        self.__line_joint_analysis(pos_to_analyze,
+                                   line_list=[STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE],
+                                   prefix=POS_OVER_RENTAL_IMG_FN_PREFIX)
+        self.__cardinal_analysis(self.st.pos, prefix=POS_OVER_RENTAL_IMG_FN_PREFIX,
+                                 line_list=[STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE],
+                                 joint_list=[STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE],
+                                 line_3d_list=[STC.POS_GEN_OVER_RENTAL_ANALYSIS_TUPLE])
         return self
 
     def heuristic_data_analysis(self):
         log.d("Test {} heuristic data analysis".format(DATASET_NAME))
+        if not self.is_heuristic_processed():
+            log.e("Test {} heuristic data analysis: you have to process heuristic earlier".format(DATASET_NAME))
+            return self
         p = HEURISTIC_IMG_FN_PREFIX
-        self.__overview_analysis(self.__filter(self.st.pos), STC.POS_GEN_HEURISTIC_COLS, prefix=p)
-        self.__cardinal_analysis(self.st.pos, prefix=p,
-                                 joint_list=[STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE,
-                                             STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE,
-                                             STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE],
-                                 line_list=[STC.POS_GEN_OVER_TIMEDELTA_ANALYSIS_TUPLE],
-                                 skip_filter=False)
+        pos_filter = self.__pos_filter(self.st.pos)
+        pos_to_analyze = self.__filter(pos_filter)
+        self.__overview_analysis(pos_to_analyze, STC.POS_GEN_HEURISTIC_ID_COLS, prefix=p + "_id")
+        self.__overview_analysis(pos_to_analyze, STC.POS_GEN_HEURISTIC_INFO_COLS, prefix=p + "_info")
+        self.__cardinal_analysis(pos_filter, prefix=p,
+                                 line_list=[STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE,
+                                            STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE,
+                                            STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE],
+                                 line_3d_list=[STC.POS_GEN_OVER_SPREADDELTA_ANALYSIS_TUPLE,
+                                               STC.POS_GEN_OVER_EDGEDELTA_ANALYSIS_TUPLE,
+                                               STC.POS_GEN_OVER_COORDDELTA_ANALYSIS_TUPLE])
+        self.__cardinal_analysis(pos_filter, prefix=p,
+                                 line_table_list=[STC.POS_GEN_OVER_TIMEDELTA_ANALYSIS_TUPLE], force_filter=True)
 
-    def maps(self, skip_filter=False):
+    def clusterized_data_analysis(self):
+        log.d("Test {} clusterized data analysis".format(DATASET_NAME))
+        if not self.is_clustering_processed():
+            log.e("Test {} clusterized data analysis: you have to process clustering earlier".format(DATASET_NAME))
+            return self
+
+        prefix = CLUSTER_IMG_FN_PREFIX
+        prefix = "" if prefix is None else "{}_".format(prefix)
+
+        dataset_for_clustering = self.data_clusterized
+        self.__line_joint_analysis(self.__filter(dataset_for_clustering), line_list=[STC.CLUSTER_ANALYSIS_TUPLE],
+                                   prefix=prefix + "all")
+        self.__cardinal_analysis(dataset_for_clustering, line_list=[STC.CLUSTER_ANALYSIS_TUPLE],
+                                 prefix=prefix + "all")
+
+        # Clustering analysis for each partition
+        partitions = self.partitions_clusterized
+        for key in partitions:
+            self.__line_joint_analysis(self.__filter(partitions[key]), prefix=prefix + key,
+                                       line_list=[STC.CLUSTER_ANALYSIS_TUPLE],
+                                       line_3d_list=[STC.CLUSTER_ANALYSIS_TUPLE])
+
+    def maps(self):
         log.d("Test {} generate maps".format(DATASET_NAME))
-        dataset_to_analyze = self.st.pos
-        if not skip_filter:
-            dataset_to_analyze = self.__filter(dataset_to_analyze)
+        if not self.is_heuristic_processed():
+            log.e("Test {} maps: you have to process heuristic earlier".format(DATASET_NAME))
+            return self
+        dataset_to_analyze = self.__pos_filter(self.st.pos)
+        dataset_to_analyze = self.__filter(dataset_to_analyze)
         da_dataset = DataAnalysis(dataset_to_analyze, dataset_to_analyze.columns, dataset_name=DATASET_NAME,
                                   save_file=SAVE_FILE)
         da_dataset.show_line_map(on=STC.POS_GEN_OVER_RENTAL_MAP_TUPLE,
-                                 hover_data=STC.POS_GEN_OVER_RENTAL_MAP_HOVER_DATA)
-        da_dataset.show_scatter_map(on=STC.POS_GEN_OVER_CLUSTER_MAP_TUPLE,
-                                    hover_data=STC.POS_GEN_OVER_CLUSTER_MAP_HOVER_DATA)
+                                 hover_data=STC.POS_GEN_OVER_RENTAL_MAP_HOVER_DATA, groupby=self.groupby)
+        da_dataset.show_scatter_map(on=STC.POS_GEN_OVER_COORDDELTA_MAP_TUPLE,
+                                    hover_data=STC.POS_GEN_OVER_COORDDELTA_MAP_HOVER_DATA)
+
+    def maps_3d(self):
+        dataset_to_analyze = self.__pos_filter(self.st.pos)
+        partitions = self.__partition(dataset_to_analyze, only_north=self.only_north)
+        for key in partitions:
+            partitions[key] = self.__filter(partitions[key])
+            da_dataset = DataAnalysis(partitions[key], partitions[key].columns, dataset_name=DATASET_NAME,
+                                      save_file=SAVE_FILE)
+            filename = "{}_3d_map.html".format(key)
+            da_dataset.show_3d_map(on=STC.POS_GEN_OVER_COORDDELTA_MAP_TUPLE, groupby=self.groupby, filename=filename)
 
     def stats(self):
         log.d("Test {} print stats".format(DATASET_NAME))
